55acf4ce746c0c14e347d3b68b336ef88551160c
Yuliia Mustafina <yuliia@shoutly.com>
2026-09-25 14:48:01 +0200

    Add counter functionality and update App component structure

--- lesson-1/src/App.tsx
+++ lesson-1/src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css'
 
 function App() {
+  let counter = 0;
+
+  const handleBtnClick = () => {
+    counter++;
+  }
 
   return (
-   <h1>01-react-basics</h1>
+    <>
+      <h1>01-react-basics</h1>
+
+      <p>Counter: {counter}</p>
+
+      <button onClick={handleBtnClick}>Click me!</button>
+    </>
   )
 }
 
