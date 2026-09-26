97ed8e3daf0a994bd2b8bea5eaf3774cfc3bdf49
Yuliia Mustafina <yuliia@shoutly.com>
2026-09-26 14:01:35 +0200

    Add Todo interface and display posts in App component

--- lesson-1/src/App.tsx
+++ lesson-1/src/App.tsx
@@ -1,10 +1,21 @@
 import { useState } from 'react';
 import './assets/scss/App.scss';
 
+interface Todo {
+  id: number;
+  title: string;
+  likes: number;
+}
+
 function App() {
   //let counter = 0;
   const [counter, setCounter] = useState(0);
   const [msg, setMsg] = useState('Hi mom!');
+  const [posts, setPosts] = useState<Todo[]>([
+    { id: 1, title: "Reackt Rocks", likes: 1324 },
+    { id: 2, title: "Learning React", likes: 256 },
+    { id: 3, title: "Advanced React Patterns", likes: 512 }
+  ]);
 
   const handleBtnClick = () => {
     setCounter(counter + 1);
@@ -23,6 +34,13 @@ function App() {
       <p>{msg}</p>
 
       <button className="btn btn-secondary" onClick={() => setMsg("Hi dad!")}>Hi dad!</button>
+
+      <hr />
+
+      <h2>Posts</h2>
+      <ul>
+        {posts.map(post => <li key={post.id}>{post.title} ({post.likes} likes)</li>)}
+      </ul>
       
     </div>
   )
